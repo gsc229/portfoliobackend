980e61188191393a6ce9a3fb24c8bfec2e9f56cd
greg <gsc229@gmail.com>
2020-06-24 03:28:02 -0400

    added requirements.txt and added the CNAME to allowed hosts aws-portfolio-backend-env.eba-e4tvkzsh.us-east-1.elasticbeanstalk.com

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn pfbe.wsgi
--- pfbe/settings.py
+++ pfbe/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['localhost','http://localhost:3000','http://127.0.0.1:8000/','http://localhost:8000','http://127.0.0.1:3000', 'https://projectsdata.herokuapp.com', 'projectsdata.herokuapp.com']
+ALLOWED_HOSTS = ['aws-portfolio-backend-env.eba-e4tvkzsh.us-east-1.elasticbeanstalk.com','localhost','http://localhost:3000','http://127.0.0.1:8000/','http://localhost:8000','http://127.0.0.1:3000', 'https://projectsdata.herokuapp.com', 'projectsdata.herokuapp.com']
 print(ALLOWED_HOSTS)
 
 # Application definition

a14546171e9bf31c22588f112a5426b9199167ec
greg <gsc229@gmail.com>
2023-05-15 19:31:29 -0600

    errors and requirements.txt

--- projects/models.py
+++ projects/models.py
@@ -5,29 +5,32 @@ from multiselectfield import MultiSelectField
 from django.contrib.postgres.fields import ArrayField
 from projects.tech_choices import TECH_CHOICES, TYPE_CHOICES
 from pygments.lexers import get_all_lexers, get_lexer_by_name
+
 # Create your models here.
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
-LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS ])
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 
 
 class Project(models.Model):
-  owner = models.ForeignKey('auth.User', related_name='projects', on_delete=models.CASCADE)
-  title = models.CharField(max_length=200, unique=True)
-  project_type = models.CharField(choices=TYPE_CHOICES, max_length=100, default='None')
-  top_photo = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
-  front_end_repo = models.CharField(max_length=500, blank=True)
-  back_end_repo = models.CharField(max_length=500, blank=True)
-  website = models.CharField(max_length=500, blank=True)
-  web_icon = models.CharField(max_length=100, blank=True)
-  description = models.TextField(blank=True)
-  roles = models.TextField(blank=True)  
-  responsibilities = models.TextField(blank=True)
-  technologies = MultiSelectField(choices=TECH_CHOICES, default='None')
-  featured = models.BooleanField(default=False)  
-  created_at = models.DateTimeField(default=datetime.now, blank=True)
-   
+    owner = models.ForeignKey(
+        "auth.User", related_name="projects", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=200, unique=True)
+    project_type = models.CharField(
+        choices=TYPE_CHOICES, max_length=100, default="None"
+    )
+    top_photo = models.ImageField(upload_to="photos/%Y/%m/%d", blank=True)
+    front_end_repo = models.CharField(max_length=500, blank=True)
+    back_end_repo = models.CharField(max_length=500, blank=True)
+    website = models.CharField(max_length=500, blank=True)
+    web_icon = models.CharField(max_length=100, blank=True)
+    description = models.TextField(blank=True)
+    roles = models.TextField(blank=True)
+    responsibilities = models.TextField(blank=True)
+    technologies = MultiSelectField(choices=TECH_CHOICES, default="None", max_length=50)
+    featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
 
-
-  def __str__(self):
-    return f"title: {self.title} created: {self.created_at}"
+    def __str__(self):
+        return f"title: {self.title} created: {self.created_at}"
--- projects/tech_choices.py
+++ projects/tech_choices.py
@@ -41,41 +41,40 @@ TECH_CHOICES = (
   )
 
 
-""" 
-class Technology(models.TextChoices):
-    PYTHON = 'PY', _('Python')
-    DJANGO = 'DJ', _('Django')
-    REACTHOOKS = 'RH', _('React with hooks')
-    REACTCLASS = 'RC', _('React with classes')
-    REACTROUTER = 'RR', _('React Router')
-    JAVASCRIPT = 'JS', _('JavaScript')
-    NODE = 'NO', _('Node')
-    EXPRESS = 'EX', _('Express')
-    MONGO = 'MG', _('MongoDB')
-    MONGOOSE = 'MS', _('Mongoose')  
-    ANGULAR = 'AG', _('Angular')  
-    POSTGRES = 'PG', _('Postgres')
-    BOOTSTAP = 'BS', _('Bootstrap')
-    HEROKU = 'HE', _('Heroku')
-    AWS = 'AW', _('Amazon Web Services')
-    DO = 'DO', _('Digital Ocean')
-    JINJA = 'JN', _('Jinja')
-    SC = 'SC', _('Styled Components')
-    CSS3 = 'C3', _('CSS 3')
-    MATERIALUI = 'MU', _('Material UI')
-    MATERIALIZE = 'MZ', _('Materialize')
-    SASS = 'SS', _('Sass')
-    LESS = 'LS', _('Less')
-    HTML5 = 'H5', _('HTML 5')
-    AXIOS = 'AX', _('Axios')
-    NONE = 'NA', _('None')
-  technologies = ArrayField(
-    models.CharField(
-      max_length=2,
-      choices=Technology.choices,
-      default=Technology.NONE
-    ),     
-    blank=True)
+
+# class Technology(models.TextChoices):
+#     PYTHON = 'PY', _('Python')
+#     DJANGO = 'DJ', _('Django')
+#     REACTHOOKS = 'RH', _('React with hooks')
+#     REACTCLASS = 'RC', _('React with classes')
+#     REACTROUTER = 'RR', _('React Router')
+#     JAVASCRIPT = 'JS', _('JavaScript')
+#     NODE = 'NO', _('Node')
+#     EXPRESS = 'EX', _('Express')
+#     MONGO = 'MG', _('MongoDB')
+#     MONGOOSE = 'MS', _('Mongoose')  
+#     ANGULAR = 'AG', _('Angular')  
+#     POSTGRES = 'PG', _('Postgres')
+#     BOOTSTAP = 'BS', _('Bootstrap')
+#     HEROKU = 'HE', _('Heroku')
+#     AWS = 'AW', _('Amazon Web Services')
+#     DO = 'DO', _('Digital Ocean')
+#     JINJA = 'JN', _('Jinja')
+#     SC = 'SC', _('Styled Components')
+#     CSS3 = 'C3', _('CSS 3')
+#     MATERIALUI = 'MU', _('Material UI')
+#     MATERIALIZE = 'MZ', _('Materialize')
+#     SASS = 'SS', _('Sass')
+#     LESS = 'LS', _('Less')
+#     HTML5 = 'H5', _('HTML 5')
+#     AXIOS = 'AX', _('Axios')
+#     NONE = 'NA', _('None')
+#   technologies = ArrayField(
+#     models.CharField(
+#       max_length=2,
+#       choices=Technology.choices,
+#       default=Technology.NONE
+#     ),     
+#     blank=True)
 
 
-"""
